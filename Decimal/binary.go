3dafc95fe30d8bb6f6d2eba1a87b5791f41c3cae
Carlos Eugenio <tec_ceugenio@uolinc.com>
2020-01-16 14:01:33 -0300

    Scripts Simples GOLANG

---
+++ Decimal/binary.go
@@ -0,0 +1,17 @@
+package main
+
+/* %b	base 2
+%c	the character represented by the corresponding Unicode code point
+%d	base 10
+%o	base 8
+%q	a single-quoted character literal safely escaped with Go syntax.
+%x	base 16, with lower-case letters for a-f
+%X	base 16, with upper-case letters for A-F
+%U	Unicode format: U+1234; same as "U+%04
+*/
+import "fmt"
+
+//%b representa numero binario
+func main() {
+	fmt.Printf("%d - %b \n", 42, 42)
+}
