0cf90ed16509a00ae6f5c5c4506ae1c333413e46
Carlos Eugenio <tec_ceugenio@uolinc.com>
2020-01-15 21:50:42 -0300

    Exemplos Golang Multiplexar

--- select/select.go
+++ select/select.go
@@ -1,10 +1,34 @@
 package main
 
-import (
+import ( 
 	"html"
 )
 
 func OmaisRapido(url1, url2, url3 string) string {
 	c1 := html.Titulo(url1)
+	c2 := html.Titulo(url2)
+	c3 := html.Titulo(url3)
 
+	//estrutura de concorrencia 
+	select {
+	case t1 := <-c1:
+		return t1
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case -< time.After(1000 * time.Millisecond):
+		return "Todos Perderam!!"
+	default:
+		return "Sem Resposta!"
+
+		}
+}
+func main() {
+	campeao := OmaisRapido(
+		"https://www.google.com",
+		"https://www.youtube.com",
+		"https://www.terra.com.br",
+	)
+	fmt.Println(campeao)
 }
