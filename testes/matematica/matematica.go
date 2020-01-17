b82ceef9df766bb4caa4cbc10f27e46c6bcf9a1c
Carlos Eugenio <tec_ceugenio@uolinc.com>
2020-01-16 21:06:58 -0300

    Exemplos de testes em goland

---
+++ testes/matematica/matematica.go
@@ -0,0 +1,16 @@
+package matematica
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func Media(numeros ...float64) float64 {
+	total := 0.000
+	for _, num := range numeros {
+		total += num
+	}
+	media := total / float64(len(numeros))
+	mediaArrendodada, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", media), 64)
+	return mediaArrendodada
+}
